707b745884dcd2d70a45c82cc20405b1a51b3d76
Teresa <luvmargaritas@gmail.com>
2022-07-28 16:16:07 -0500

    added horned beast js

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Card, Button, Col} from 'react-bootstrap'
+import { Card, Button, Col } from 'react-bootstrap'
 
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      beastVote: false
+    };
+  }
+
+  beastVotes = () => {
+    this.setState({
+      beastVote: true
+    });
+  }
+
+  votesGot = () => {
+    this.setState({
+      beastVote: false
+    });
+  }
   render() {
-    
-    console.log('This props', this.props)
     return (
-      <div>
-       <h2>{this.props.title}</h2> 
-       <img src={this.props.image_url} alt={this.props.title}/>
-       <p>{this.props.description}</p>
-      </div>
+      <>
+        <Col className="mt-4">
+          <Card className="h-100 p-3">
+            <Card.Title onClick={this.helperFunctionH3Click} >{this.props.beastName}</Card.Title>
+            <Card.img src={this.props.image_url} alt={this.props.title} onClick={this.props.addBeast} />
+            <p>{this.props.description}</p>
+            <p>{this.state.likes} Like</p>
+            <p>onClick={this.handleLikes}>Click to Like your favorite Beast</p>
+            <div>{this.state.beastVote ? 'Vote for your favorite Horned Beast' : ''}</div>
+              <Button onClick={this.beastVote}>Nice choice!</Button>
+              <Button variant="success" onClick={this.votesGot}>Your vote was counted</Button>
+          </Card>
+        </Col>
+      </>
     );
   }
 }
